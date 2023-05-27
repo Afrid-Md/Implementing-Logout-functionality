1e6c86648f70fa468e72b9911ed3830271f5d0bb
afrid.md <122868488+Afrid-Md@users.noreply.github.com>
2023-05-27 15:02:08 +0530

    Addressing security and logout issues

---
+++ authContext-provider.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import AuthContext from "./auth-context";
+
+const AuthContextProvider=(props)=> {
+  const initialToken=localStorage.getItem('token');
+  const [token, setToken] = useState(initialToken);
+
+  const userIsLoggedIn = !!token;
+
+  const logInHandler = (token) => {
+    setToken(token);
+    localStorage.setItem('token', token);
+  };
+
+  const logOutHandler = () => {
+    setToken(null);
+    localStorage.removeItem('token');
+  };
+
+  const authcontext = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: logInHandler,
+    logout: logOutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={authcontext}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+export default AuthContextProvider;
